bf96871a2f384e5031295d395721c334caf00d6a
XunNam <hoang.xuannam@outlook.com>
2024-09-25 09:10:53 +0700

    Tạo trang Web giới thệu
    Tạo trang web giới thiệu bản thân, có repsonsive và đã format bằng prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'github-dark': '#0d1117',
+        'github-gray': '#3d444d',
+      }
+    },
   },
   plugins: [],
 }
